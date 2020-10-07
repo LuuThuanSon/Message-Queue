0eade72a55a1f491cbdcb4891b695f32341ffb65
Lưu Thuần Sơn <luuthuansonit@gmail.com>
2020-10-07 15:17:57 +0700

    add producer and consumer

---
+++ MessageQueue.Consumer/MessageQueue.Consumer/Program.cs
@@ -0,0 +1,92 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageQueue.Consumer
+{
+    public class CustomTaskScheduler : TaskScheduler
+    {
+        // ...
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.VirtualHost = "/";
+            factory.HostName = "localhost";
+            factory.Port = 5672;
+            //factory.DispatchConsumersAsync = true;
+            //factory.AutomaticRecoveryEnabled = true;
+            //factory.TaskScheduler = new CustomTaskScheduler();
+            //factory.ClientProvidedName = "app:audit component:event-consumer"
+            using (var connection = factory.CreateConnection())
+            {
+
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+                    channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                    //string tag = channel.BasicConsume("hello", false, consumer);
+                    //// this consumer tag identifies the subscription
+                    //// when it has to be cancelled
+
+
+                    //bool noAck = false;
+
+                    //BasicGetResult result = channel.BasicGet("hello", noAck);
+                    //if (result == null)
+                    //{
+                    //    // No message available at this time.
+                    //}
+                    //else
+                    //{
+                    //    IBasicProperties props = result.BasicProperties;
+                    //    ReadOnlyMemory<byte> body = result.Body;
+                    //    channel.BasicAck(result.DeliveryTag, false);
+                    //}
+                    // ensure we get a delivery
+                    //bool waitRes = latch.WaitOne(2000);
+                }
+            }
+        }
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MessageQueue.Producer/MessageQueue.Producer/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace MessageQueue.Producer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.VirtualHost = "/";
+            factory.HostName = "localhost";
+            factory.Port = 5672;
+            //factory.ClientProvidedName = "app:audit component:event-consumer"
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                    //channel.QueueDeclare(queueName, false, false, false, null);
+                    //channel.QueueBind(queueName, exchangeName, routingKey, null);
+                    //channel.QueueDeclareNoWait(queueName, true, false, false, null);
+                    //channel.QueueDelete("queue-name", false, false);
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+                    byte[] body = System.Text.Encoding.UTF8.GetBytes("Hello, world!");
+                    IBasicProperties props = channel.CreateBasicProperties();
+                    props.ContentType = "text/plain";
+                    props.DeliveryMode = 2;
+                    props.Expiration = "36000000";
+                    //props.Headers = new Dictionary<string, object>();
+                    //props.Headers.Add("latitude", 51.5252949);
+                    //props.Headers.Add("longitude", -0.0905493);
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: props,
+                                         body: body);
+                    Console.WriteLine("Send Hello World !");
+                    Console.ReadLine();
+                    //var response = channel.QueueDeclarePassive("queue-name");
+                    //// returns the number of messages in Ready state in the queue
+                    //response.MessageCount;
+                    //// returns the number of consumers the queue has
+                    //response.ConsumerCount;
+                }
+            }
+        }
+    }
+}
